cafb96b874ca996749d11944c22ca40d8db1c847
nhulnt <nhule27082003@gmail.com>
2024-12-28 21:40:49 +0700

    nhulnt-task1-1st Backend code for task medicine management include init healthcare database and medicine management code

---
+++ PSBS.HealthCareServiceApiSolution/PSBS.HealthCareApi.Application/DTOs/Conversions/MedicineConversion.cs
@@ -0,0 +1,49 @@
+﻿using PSBS.HealthCareApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSBS.HealthCareApi.Application.DTOs.Conversions
+{
+    public static class MedicineConversion
+    {
+        public static Medicine ToEntity(MedicineDTO medicineDTO, string? imagePath = null) => new()
+        {
+            medicineId = medicineDTO.medicineId,
+            treatmentId = medicineDTO.treatmentId,
+            medicineName = medicineDTO.medicineName,
+            medicineImage = imagePath ?? medicineDTO.medicineImage,
+            isDeleted = medicineDTO.isDeleted
+        };
+
+        public static (MedicineDTO?, IEnumerable<MedicineDTO>?) FromEntity(Medicine medicine, IEnumerable<Medicine>? medicines)
+        {
+            //return single
+            if (medicine != null || medicines == null)
+            {
+                var singleMedicine = new MedicineDTO
+                    (
+                        medicine!.medicineId,
+                        medicine.treatmentId,
+                        medicine.medicineName,
+                        medicine.medicineImage,
+                        null,
+                        medicine.isDeleted
+                    );
+                return (singleMedicine, null);
+            }
+
+            //return list
+            if (medicines != null || medicine == null)
+            {
+                var listMedicines = medicines!.Select(m =>
+                    new MedicineDTO(m.medicineId, m.treatmentId, m.medicineName, m.medicineImage, null, m.isDeleted)).ToList();
+                return (null, listMedicines);
+            }
+
+            return (null, null);
+        }
+    }
+}
---
+++ PSBS.HealthCareServiceApiSolution/PSBS.HealthCareApi.Infrastructure/Data/HealthCareDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PSBS.HealthCareApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSBS.HealthCareApi.Infrastructure.Data
+{
+    public class HealthCareDbContext : DbContext
+    {
+        public HealthCareDbContext(DbContextOptions<HealthCareDbContext> options) : base(options) { }
+        public DbSet<Medicine> PetHealthBooks { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<Treatment> Treatments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PetHealthBook>().HasKey(phb => phb.healthBookId);
+            modelBuilder.Entity<PetHealthBook>()
+                .HasOne(phb => phb.Medicine)
+                .WithOne(m => m.PetHealthBook)
+                .HasForeignKey<PetHealthBook>(phb => phb.medicineId);
+
+            modelBuilder.Entity<Medicine>().HasKey(m => m.medicineId);
+            modelBuilder.Entity<Medicine>()
+                .HasOne(m => m.Treatment)
+                .WithMany(t => t.Medicines)
+                .HasForeignKey(m => m.treatmentId);
+
+            modelBuilder.Entity<Treatment>().HasKey(t => t.treatmentId);
+        }
+    }
+}
