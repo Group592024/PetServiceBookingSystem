0fe5557a0f28564895928f454e7f330527e5201f
bettyTS13 <hienhtce171718@fpt.edu.vn>
2024-12-28 18:18:51 +0700

    Complete Code Back End For Voucher

---
+++ PSBS.RewardServiceApiSolution/VoucherApi.Infrastructure/Data/Migarations/20241228105427_Init.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoucherApi.Infrastructure.Data.Migarations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gifts",
+                columns: table => new
+                {
+                    gift_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    gift_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    gift_description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    gift_image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    gift_point = table.Column<int>(type: "int", nullable: false),
+                    gift_code = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    gift_status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gifts", x => x.gift_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vouchers",
+                columns: table => new
+                {
+                    voucher_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    voucher_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    voucher_description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    voucher_quantity = table.Column<int>(type: "int", nullable: false),
+                    voucher_discount = table.Column<int>(type: "int", nullable: false),
+                    voucher_maximum = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    voucher_minimum_spend = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    voucher_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    voucher_start_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    voucher_end_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    is_gift = table.Column<bool>(type: "bit", nullable: false),
+                    is_deleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vouchers", x => x.voucher_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RedeemGiftHistories",
+                columns: table => new
+                {
+                    redeemhistory_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    gift_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    account_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    redeem_point = table.Column<int>(type: "int", nullable: false),
+                    redeem_date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RedeemGiftHistories", x => x.redeemhistory_id);
+                    table.ForeignKey(
+                        name: "FK_RedeemGiftHistories_Gifts_gift_id",
+                        column: x => x.gift_id,
+                        principalTable: "Gifts",
+                        principalColumn: "gift_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RedeemGiftHistories_gift_id",
+                table: "RedeemGiftHistories",
+                column: "gift_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RedeemGiftHistories");
+
+            migrationBuilder.DropTable(
+                name: "Vouchers");
+
+            migrationBuilder.DropTable(
+                name: "Gifts");
+        }
+    }
+}
---
+++ PSBS.RewardServiceApiSolution/VoucherApi.Infrastructure/Repositories/VoucherRepository.cs
@@ -0,0 +1,150 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using PSPS.SharedLibrary.PSBSLogs;
+using PSPS.SharedLibrary.Responses;
+using System.Linq.Expressions;
+using VoucherApi.Application.Interfaces;
+using VoucherApi.Domain.Entities;
+using VoucherApi.Infrastructure.Data;
+
+namespace VoucherApi.Infrastructure.Repositories
+{
+    public class VoucherRepository(RewardServiceDBContext context) : IVoucher
+    {
+        public async Task<Response> CreateAsync(Voucher entity)
+        {
+            try
+            {
+                var getVoucher = await GetByAsync(p => p.VoucherName!.Equals(entity.VoucherName) && !p.IsDeleted);
+                if (getVoucher is not null && !string.IsNullOrEmpty(getVoucher.VoucherName))
+                    return new Response(false, $"{entity.VoucherName} already added");
+
+                var getVoucherCode = await GetByAsync(p => p.VoucherCode!.Equals(entity.VoucherCode) && !p.IsDeleted);
+                if (getVoucherCode is not null && !string.IsNullOrEmpty(getVoucherCode.VoucherName))
+                    return new Response(false, $"The Code {entity.VoucherCode} already added");
+
+                var currentEntity = context.Vouchers.Add(entity).Entity;
+                await context.SaveChangesAsync();
+                if (currentEntity is not null && currentEntity.VoucherId.ToString().Length > 0)
+                {
+                    return new Response(true, $"{entity.VoucherName} added to database successfully");
+                }
+                else
+                {
+                    return new Response(false, $"{entity.VoucherName} cannot be added due to errors");
+                }
+            }
+            catch (Exception ex)
+            {
+                // log the orginal exception
+                LogExceptions.LogException(ex);
+                // display scary-free message to the client
+                return new Response(false, "Error occured adding new voucher");
+            }
+        }
+
+        public async Task<Response> DeleteAsync(Voucher entity)
+        {
+            try
+            {
+
+                var voucher = await GetByIdAsync(entity.VoucherId);
+                if (voucher is null)
+                {
+                    return new Response(false, $"{entity.VoucherName} not found");
+                }
+                entity.IsDeleted = true;
+                context.Entry(voucher).State = EntityState.Detached;
+                context.Vouchers.Update(entity);
+                await context.SaveChangesAsync();
+                return new Response(true, $"{entity.VoucherName} successfully deleted");
+            }
+            catch (Exception ex)
+            {
+                // log the orginal exception
+                LogExceptions.LogException(ex);
+                // display scary-free message to the client
+                return new Response(false, "Error occurred updating existing voucher");
+            }
+        }
+
+        public async Task<IEnumerable<Voucher>> GetAllAsync()
+        {
+            try
+            {
+                var vouchers = await context.Vouchers
+                    .AsNoTracking()
+                    .Where(v => !v.IsDeleted) // Filter where isDeleted is false
+                    .ToListAsync();
+                return vouchers;
+            }
+            catch (Exception ex)
+            {
+                // Log the original exception
+                LogExceptions.LogException(ex);
+                // Display a client-friendly error message
+                throw new Exception("Error occurred while retrieving vouchers.");
+            }
+        }
+
+
+        public async Task<Voucher> GetByAsync(Expression<Func<Voucher, bool>> predicate)
+        {
+            try
+            {
+                var voucher = await context.Vouchers.Where(predicate).FirstOrDefaultAsync()!;
+                return voucher is not null ? voucher : null!;
+            }
+            catch (Exception ex)
+            {
+                // log the orginal exception
+                LogExceptions.LogException(ex);
+                // display scary-free message to the client
+                throw new Exception("Error occurred retrieving voucher");
+            }
+        }
+
+        public async Task<Voucher> GetByIdAsync(Guid id)
+        {
+            try
+            {
+                var voucher = await context.Vouchers
+                    .AsNoTracking()
+                    .SingleOrDefaultAsync(v => v.VoucherId == id && !v.IsDeleted); // Filter by ID and isDeleted
+                return voucher!;
+            }
+            catch (Exception ex)
+            {
+                // Log the original exception
+                LogExceptions.LogException(ex);
+                // Display a client-friendly error message
+                throw new Exception("Error occurred retrieving voucher.");
+            }
+        }
+
+
+        public async Task<Response> UpdateAsync(Voucher entity)
+        {
+            try
+            {
+                var voucher = await GetByIdAsync(entity.VoucherId);
+                if (voucher is null)
+                {
+                    return new Response(false, $"{entity.VoucherName} not found");
+                }
+                context.Entry(voucher).State = EntityState.Detached;
+                context.Vouchers.Update(entity);
+                await context.SaveChangesAsync();
+                return new Response(true, $"{entity.VoucherName} successfully updated");
+            }
+            catch (Exception ex)
+            {
+                // log the orginal exception
+                LogExceptions.LogException(ex);
+                // display scary-free message to the client
+                return new Response(false, "Error occurred updating existing voucher");
+            }
+        }
+    }
+}
