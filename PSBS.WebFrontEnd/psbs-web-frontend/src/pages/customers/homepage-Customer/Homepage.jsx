fd2de5b58c1e931b17958daf34ea61908ec8f10a
bettyTS13 <hienhtce171718@fpt.edu.vn>
2025-01-05 21:04:34 +0700

    install tailwind, mui package and sweetalert

--- PSBS.WebFrontEnd/psbs-web-frontend/src/pages/customers/homepage-Customer/Homepage.jsx
+++ PSBS.WebFrontEnd/psbs-web-frontend/src/pages/customers/homepage-Customer/Homepage.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import NavbarCustomer from '../../../components/navbar-customer/NavbarCustomer'
+import React from "react";
+import NavbarCustomer from "../../../components/navbar-customer/NavbarCustomer";
 
 const Homepage = () => {
   return (
-    <div><NavbarCustomer/>
-    
-    <div className="content">haha</div></div>
-    
-  )
-}
+    <div>
+      <NavbarCustomer />
 
-export default Homepage
+      <div className="content">
+        <h1 className="text-3xl font-bold underline text-blue-500 hover:text-red-500">
+          Hello world!
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
