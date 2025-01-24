df9e99d389dd3a742b884f9615c9eb60b99c97c5
Duyenltmce23 <ltmduyen2k3@gmail.com>
2025-01-24 22:55:13 +0700

    BE for pet diary

---
+++ PSBS.PetServiceApiSolution/PetApi.Application/Interfaces/IPetDiary.cs
@@ -0,0 +1,10 @@
+﻿using PetApi.Domain.Entities;
+using PSPS.SharedLibrary.Interface;
+
+namespace PetApi.Application.Interfaces
+{
+    public interface IPetDiary : IGenericInterface<PetDiary>
+    {
+        Task<IEnumerable<PetDiary>> GetAllDiariesByPetIdsAsync(Guid id);
+    }
+}
