552b9f034e207999f3878d0bc3813a7c23cb6a73
Duyenltmce23 <ltmduyen2k3@gmail.com>
2025-02-05 13:38:52 +0700

    report about staff, customer, number of services and rooms by type

---
+++ PSBS.AccountServiceApiSolution/PSPS.AuthenticationAPI.Presentation/Controllers/ReportAccountController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using PSPS.AccountAPI.Application.DTOs;
+using PSPS.AccountAPI.Application.Interfaces;
+
+namespace PSPS.AccountAPI.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ReportAccountController(IAccount account) : ControllerBase
+    {
+
+        [HttpGet("countStaff")]
+        public async Task<ActionResult<List<GetAccountDTO>>> GetAllStaffs()
+        {
+            var result = await account.GetAllStaffAccount();
+            if (result == null)
+                return NotFound(new { Message = "No accounts found" });
+
+            return Ok(result);
+        }
+
+        [HttpGet("countCustomer")]
+        public async Task<ActionResult<List<GetAccountDTO>>> GetAllCustomers()
+        {
+            var result = await account.GetAllCustomerAccount();
+            if (result == null)
+                return NotFound(new { Message = "No accounts found" });
+
+            return Ok(result);
+        }
+    }
+}
--- PSBS.FacilityServiceApiSolution/FacilityServiceApi.Infrastructure/Data/20250204171303_second.Designer.cs
+++ PSBS.FacilityServiceApiSolution/FacilityServiceApi.Infrastructure/Data/20250204171303_second.Designer.cs
@@ -4,6 +4,7 @@ using FacilityServiceApi.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FacilityServiceApi.Infrastructure.Data
 {
     [DbContext(typeof(FacilityServiceDbContext))]
-    partial class FacilityServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250204171303_second")]
+    partial class second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
